ca534138db72305d7cbcd98d6b666c90154b867e
Jamy Timmermans <me@jamy.be>
2018-04-02 03:06:02 +0200

    chore(travis): mocha wants at least 1 unit

---
+++ loauth-api/src/index.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+// import { hello } from './';
+describe('index', () => {
+    it('returns true', () => {
+        // expect(hello()).to.equal('hello world');
+    });
+});
